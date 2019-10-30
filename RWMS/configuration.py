3ae31506aaa0393471e3afca31e9546ab0a82cd6
Jacob Rivera <edjacob25@gmail.com>
2019-10-29 18:15:45 -0600

    Moved config module to use Path api

--- RWMS/configuration.py
+++ RWMS/configuration.py
@@ -5,39 +5,42 @@ configuration module for RWMS
 import configparser
 import os
 import sys
+from pathlib import Path
+from typing import Optional, Union
 
 if sys.platform == "win32":
     import winreg
 
 
-def configuration_file():
+def configuration_file() -> Path:
     """
     returns the configuration file as a string, empty if not detected.
     :return: str
     """
 
     # check, if script is compiled with pyinstaller
-    mypath = str()
-
     if getattr(sys, 'frozen', False):
         mypath = os.path.dirname(sys.executable)
+        base_path = Path(".").parent
     elif __file__:
         mypath = os.path.dirname(sys.argv[0])
+        base_path = Path.cwd()
 
-    return os.path.join(mypath, "rwms_config.ini")
+    return base_path / "rwms_config.ini"
+    # return os.path.join(mypath, "rwms_config.ini")
 
 
-def load_value(section, entry, isBool=False):
+def load_value(section, entry, is_bool=False) -> Union[str, bool]:
     """
-    loads a value from the configurato
+    loads a value from the configurator
     :param section: configuration file section
     :param entry: entry
-    :param isBool: optional, if it is a boolean switch
+    :param is_bool: optional, if it is a boolean switch
     :return: value
     """
     configfile = configuration_file()
 
-    if not os.path.isfile(configfile):
+    if not configfile.is_file():
         return ""
 
     cfg = configparser.ConfigParser()
@@ -49,7 +52,7 @@ def load_value(section, entry, isBool=False):
         sys.exit(1)
 
     try:
-        if isBool:
+        if is_bool:
             value = cfg.getboolean(section, entry)
         else:
             value = cfg.get(section, entry, raw=True)
@@ -60,130 +63,139 @@ def load_value(section, entry, isBool=False):
     return value
 
 
-def detect_steam():
+def detect_steam() -> Optional[Path]:
     """
     automatic detection of steam
     :return: path to steam base directory
     """
     disablesteam = load_value("rwms", "disablesteam", True)
     if disablesteam:
-        return ""
-    steampath = load_value("paths", "steamdir")
-    key = None
-    if steampath == "":
+        return None
+    steam_path = load_value("paths", "steamdir")
+    if steam_path == "":
         if sys.platform == "win32":
             registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+            key = None
             if registry:
                 try:
                     key = winreg.OpenKey(registry, r"SOFTWARE\WoW6432Node\Valve\Steam")
                 except:
-                    steampath = ""
+                    steam_path = None
                 if key:
-                    steampath, _ = winreg.QueryValueEx(key, "InstallPath")
+                    res, _ = winreg.QueryValueEx(key, "InstallPath")
+                    steam_path = Path(res)
             winreg.CloseKey(registry)
         elif sys.platform == "darwin":
-            steampath = os.environ["HOME"] + "/Library/Application Support/Steam"
+            steam_path = Path.home() / "Library/Application Support/Steam"
         elif sys.platform == "linux":
-            steampath = os.environ["HOME"] + "/.steam/steam"
-    return steampath
+            steam_path = Path.home() / ".steam/steam"
+        return steam_path
+    else:
+        return Path(steam_path)
 
 
-def detect_rimworld_steam():
-    rwsteampath = detect_steam()
-    if rwsteampath != "":
+def detect_rimworld_steam() -> Optional[Path]:
+    rw_steam_path = detect_steam()
+    if rw_steam_path is not None:
         if sys.platform == "win32":
-            rwsteampath = os.path.join(rwsteampath, "steamapps", "common", "RimWorld")
+            rw_steam_path = rw_steam_path / "steamapps/common/RimWorld"
         elif sys.platform == "darwin":
-            rwsteampath = os.path.join(rwsteampath, "steamapps", "common", "RimWorld", "RimWorldMac.app")
+            rw_steam_path = rw_steam_path / "steamapps/common/RimWorld/RimWorldMac.app"
         elif sys.platform == "linux":
-            rwsteampath = os.path.join(rwsteampath, "steamapps", "common", "RimWorld")
-    return rwsteampath
+            rw_steam_path = rw_steam_path / "steamapps/common/RimWorld"
+    return rw_steam_path
 
-def detect_rimworld_local():
+
+def detect_rimworld_local() -> Path:
     """
-    detects local drmfree RimWorld installation (has to be configured via configuration file)
+    detects local drm free RimWorld installation (has to be configured via configuration file)
     :return: path to RimWorld installation
     """
-    drmfreepath = load_value("paths", "drmfreedir")
-    return drmfreepath
+    drm_freepath = load_value("paths", "drmfreedir")
+    return Path(drm_freepath)
 
 
-def detect_rimworld():
+def detect_rimworld() -> Path:
     """
     generic detection of RimWorld installation
     :return: path to RimWorld installation
     """
     path = detect_rimworld_steam()
-    if path == "":
+    if path is None:
         path = detect_rimworld_local()
     return path
 
 
-def detect_rimworld_configdir():
+def detect_rimworld_configdir() -> Path:
     """
     detects RimWorld configuration directory (savegames etc)
     :return: path to RimWorld configuration
     """
-    rimworld_configdir = load_value("paths", "configdir")
-    if rimworld_configdir == "":
+    rimworld_config_dir = load_value("paths", "configdir")
+    if rimworld_config_dir == "":
         if sys.platform == "win32":
-            rimworld_configdir = os.environ[
-                                     "USERPROFILE"] + r"\AppData\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Config"
+            rimworld_config_dir = Path.home() / "AppData/LocalLow/Ludeon Studios/RimWorld by Ludeon Studios/Config"
         elif sys.platform == "linux":
-            rimworld_configdir = os.environ["HOME"] + "/.config/unity3d/Ludeon Studios/RimWorld by Ludeon Studios/Config"
+            rimworld_config_dir = Path.home() / ".config/unity3d/Ludeon Studios/RimWorld by Ludeon Studios/Config"
         elif sys.platform == "darwin":
-            rimworld_configdir = os.environ["HOME"] + "/Library/Application Support/RimWorld/Config"
-    return rimworld_configdir
+            rimworld_config_dir = Path.home() / "Library/Application Support/RimWorld/Config"
+    else:
+        return Path(rimworld_config_dir)
+    return rimworld_config_dir
 
 
-def detect_steamworkshop_dir():
+def detect_steamworkshop_dir() -> Optional[Path]:
     """
     detects steamworkshop directory if steam version
     :return: path to workshop directory
     """
     if load_value("rwms", "disablesteam", True):
-        return ""
-    modsdir = load_value("paths", "workshopdir")
-    if modsdir == "":
-        modsdir = os.path.join(detect_steam(), "steamapps", "workshop", "content", "294100")
-    return modsdir
+        return None
+    mods_dir = load_value("paths", "workshopdir")
+    if mods_dir == "":
+        mods_dir = detect_steam() / "steamapps/workshop/content/294100"
+    else:
+        mods_dir = Path(mods_dir)
+    return mods_dir
 
 
-def detect_localmods_dir():
+def detect_localmods_dir() -> Optional[Path]:
     """
     detects local mods directory for RimWorld
     :return: path to localmods directory
     """
-    modsdir = load_value("paths", "localmodsdir")
-    if modsdir == "":
-        steampath = detect_rimworld_steam()
-        if steampath != "":
-            modsdir = os.path.join(steampath, "Mods")
+    mods_dir = load_value("paths", "localmodsdir")
+    if mods_dir == "":
+        steam_path = detect_rimworld_steam()
+        if steam_path is not None:
+            mods_dir = steam_path / "Mods"
         else:
-            drmfreepath = detect_rimworld_local()
-            if drmfreepath != "":
-                os.path.join(drmfreepath, "Mods")
-    return modsdir
+            drm_free_path = detect_rimworld_local()
+            if drm_free_path.exists():
+                mods_dir = drm_free_path / "Mods"
+        return mods_dir
+    else:
+        return None
 
 
-def modsconfigfile():
+def modsconfigfile() -> Path:
     """
     ModsConfig.xml
     :return: returns full path of ModsConfig.xml
     """
-    return os.path.join(detect_rimworld_configdir(), "ModsConfig.xml")
+    return detect_rimworld_configdir() / "ModsConfig.xml"
 
 
-def __check_dir(path):
-    if os.path.isdir(path):
-        return "OK " + path
+def __check_dir(path: Path) -> str:
+    if path.is_dir():
+        return f"OK {path}"
     else:
         return f"ERR {path}"
 
 
-def __check_file(file):
-    if os.path.isfile(file):
-        return "OK " + file
+def __check_file(file: Path) -> str:
+    if file.is_file():
+        return f"OK {file}"
     else:
         return f"ERR {file}"
 
@@ -197,7 +209,7 @@ def __dump_configuration():
     if getattr(sys, 'frozen', False):
         # we are running in a bundle
         frozen = 'ever so'
-        bundle_dir = sys._MEIPASS # pylint: disable=no-member
+        bundle_dir = sys._MEIPASS  # pylint: disable=no-member
     else:
         # we are running in a normal Python environment
         bundle_dir = os.path.dirname(os.path.abspath(__file__))
@@ -210,7 +222,7 @@ def __dump_configuration():
     print("")
     print("configuration file is {}\n".format(configuration_file()))
     print("Current OS agnostic configuration")
-    if detect_rimworld_steam() != "":
+    if detect_rimworld_steam() is not None:
         print("")
         print("Steam is on .....................: " + __check_dir(detect_steam()))
         print("")
